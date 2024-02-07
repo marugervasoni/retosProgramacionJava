40e0adb8ada9d20de3fd8dfc7bf699b3eecc7ecd
marugervasoni <gervasonimaria15@gmail.com>
2024-02-06 22:31:18 -0300

    ejercicio 1

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        https://www.java.com/es/
+
+
+//        comentario en una linea
+
+        /*comentario
+          en
+          varias
+          lineas
+           */
+
+
+        int number = 7;
+        final int NUMBER_CONST;
+        NUMBER_CONST = 8;
+
+
+        byte tipoDatoByte = 127;
+        short tipoDatoShort = 32767;
+        int tipoDatoInt = 5;
+        long tipoDatoLong = 1000;
+        float tipoDatoFloat = 2.5f;
+        double tipoDatoDouble = 2.4;
+        boolean tippDatoBoolean = true;
+        char tipoDatoChar = 'a';
+        String tipoDatoString = "soy un string";
+
+
+        System.out.println("Hola Java");
+    }
+}
